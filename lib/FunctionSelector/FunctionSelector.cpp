c43bdacb2e6b324fdb8d1c59055240594f437fb0
labowers <lwbowers@yahoo.com>
2025-09-22 16:59:06 -0400

    updated to add knob

--- lib/FunctionSelector/FunctionSelector.cpp
+++ lib/FunctionSelector/FunctionSelector.cpp
@@ -2,6 +2,11 @@
 
 #define DIAL_RADIUS 200 // Should be even
 #define SELECTOR_SIZE 152
+#define TM_WIDTH 210
+#define TM_HEIGHT 50
+
+// Variables
+bool use_random_values = false;
 
 struct point_t
 {
@@ -12,18 +17,20 @@ struct point_t
 FunctionSelector::FunctionSelector(TFT_eSPI &display) {
   this->_display = &display;
   this->_selectorSize = SELECTOR_SIZE;
+  this->_dialOffset = display.width() - 25; // Offset from right edge
 }
 
 void FunctionSelector::begin() {
     _selectorSprite = new TFT_eSprite(_display);
     _selectorSprite->createSprite(SELECTOR_SIZE, DIAL_RADIUS);
     _selectorSprite->fillSprite(TFT_GREEN);
+    _knobSprite = new TFT_eSprite(_display);
+    _textArea = new TFT_eSprite(_display);     
 }
 
 void FunctionSelector::drawSelector(int32_t x, int32_t y) {
     drawDial();
-    _selectorSprite->drawRect(0, 0, SELECTOR_SIZE, DIAL_RADIUS, TFT_WHITE);
-    _selectorSprite->pushSprite(x, y);
+    setKnob(330);
 }
 
 void FunctionSelector::drawDial() {
@@ -74,3 +81,133 @@ void FunctionSelector::drawDial() {
 
     _selectorSprite->setPivot(dial_center.x, dial_center.y);
 }
+
+void FunctionSelector::createKnob(int16_t diameter = 100) {
+    int32_t r = diameter / 2; // Radius = Diameter/2
+    point_t center = {r, r};  // Center
+
+    Serial.println("Creating knob");
+    _knobSprite->createSprite(diameter, diameter);
+    _knobSprite->fillSprite(TFT_GREEN); // Green will be set as transparent background)
+
+    // Draw Knob
+    // Base of knob has a radius that is 10 pixels less than the  total radius
+    // The pointer of the knob extends to the radius
+    _knobSprite->fillCircle(center.x, center.y, r, TFT_BLACK);          // Knob+Pointer diameter (was  TFT_LIGHTGREY)
+    _knobSprite->fillCircle(center.x, center.y, r - 12, TFT_LIGHTGREY); // knob (was TFT_DARKGREY)
+    _knobSprite->drawCircle(center.x, center.y, r - 12, TFT_BLACK);     // Border
+    _knobSprite->drawCircle(center.x, center.y, r - 16, TFT_BLACK);     // Border
+
+    // Pointer
+    _knobSprite->drawWedgeLine(center.x, center.y, center.x, 2, 8, 3, TFT_RED); // ax, ay, bx, by, aw, bw, color
+    _knobSprite->fillCircle(center.x, center.y, 3, TFT_BLACK);
+}
+
+void FunctionSelector::setKnob(int16_t angle) {
+    static int16_t previous_angle;
+
+    if (angle == previous_angle)
+        return;
+
+    Serial.printf("Setting knob to angle %d\n", angle);
+    previous_angle = angle;
+
+    if (!_knobSprite->created()) {
+        createKnob(100);
+    }
+
+    // redraw_labels(angle);
+    _selectorSprite->setPivot(100, 100);
+    _knobSprite->pushRotated(_selectorSprite, angle, TFT_GREEN); // Push/Rotate knob to dial.  Render Green pixels as transparent
+    _selectorSprite->pushSprite(_dialOffset, 1);
+        
+}
+
+void FunctionSelector::updateValue(int selval) {
+    switch (selval)
+    {
+    case 1:
+        // showAirspeed(int(APAirspeed));
+        break;
+    case 2:
+        // showHeading(int(APCourse));
+        break;
+    case 3:
+        // showHeading(int(APHeading));
+        break;
+    case 4:
+        // showVspeed(int(APVertSpeed));
+        break;
+    case 5:
+        // showAltitude(int(APAltitude));
+        break;
+    }
+}
+
+void FunctionSelector::updateFunction(int selval) {
+    int ival;
+    long lval;
+    char msgText[24];               // For messages
+
+    ival = 180 + (selval * 30); // The angle to draw the pointer
+    switch (selval)
+    {
+    case 5:
+        sprintf(msgText, "Altitude");
+        showMessage(msgText);
+        setKnob(ival);
+        updateValue(selval);
+        break;
+
+    case 4:
+        sprintf(msgText, "Vertical Speed");
+        showMessage(msgText);
+        setKnob(ival);
+        updateValue(selval);
+        break;
+
+    case 3:
+        sprintf(msgText, "Heading");
+        showMessage(msgText);
+        setKnob(ival);
+        updateValue(selval);
+        break;
+
+    case 2:
+        sprintf(msgText, "Course");
+        showMessage(msgText);
+        setKnob(ival);
+        updateValue(selval);
+        break;
+
+    case 1:
+        sprintf(msgText, "Air Speed");
+        showMessage(msgText);
+        setKnob(ival);
+        updateValue(selval);
+        break;
+
+    default:
+        sprintf(msgText, "INVALID: %d", selval);
+        showMessage(msgText);
+    }
+}
+
+void FunctionSelector::showMessage(char *message)
+{
+    char message2[40];
+    point_t msgdisp_loc = {1, 1};
+
+    if (!_textArea->created()) {
+        _textArea->createSprite(TM_WIDTH, TM_HEIGHT);
+    }
+
+    _textArea->fillSprite(TFT_BLACK);
+    _textArea->setTextDatum(MC_DATUM); // MC_DATUM=Middle-Center
+    _textArea->setTextColor(TFT_WHITE);
+    _textArea->setTextFont(4); // 4
+    _textArea->drawString(message, TM_WIDTH / 2, TM_HEIGHT / 2);
+    _textArea->drawRect(1, 1, TM_WIDTH-1, TM_HEIGHT-1, TFT_WHITE);
+    _textArea->pushSprite(0, 0); // Replace 0,0 with your desired location
+}
+
--- lib/FunctionSelector/FunctionSelector.h
+++ lib/FunctionSelector/FunctionSelector.h
@@ -15,13 +15,24 @@ public:
   FunctionSelector(TFT_eSPI &display);
   void begin();
   void drawSelector(int32_t x, int32_t y);
+  void updateFunction(int selval);
 
 private:
   TFT_eSPI *_display;
   TFT_eSprite* _selectorSprite;
+  TFT_eSprite* _knobSprite;
+  TFT_eSprite* _textArea;
+
   int16_t _selectorSize;
+  int32_t _dialOffset;
+
 
   void drawDial();
+  void createKnob(int16_t diameter);
+  void setKnob(int16_t angle); 
+  void updateValue(int selval);
+  void showMessage(char *message);
+  
 };
 
 #endif // __FUNCTIONSELECTOR_H__
